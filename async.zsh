a02ebaf26d62c0d9393acadd47e37ae084f4adc9
Matthew Edwards <mje-nz@users.noreply.github.com>
2019-10-28 11:44:44 +1300

    Cache working dir block in async prompt

--- async.zsh
+++ async.zsh
@@ -18,7 +18,14 @@ source "$__MJE_THEME_DIR/src/prompt_common.zsh"
 
 # Print the working directory
 prompt_working_dir_block_fast() {
-  prompt_working_dir_part "$(shrink_path)"
+  typeset -gA __PROMPT_WORKING_DIR_BLOCK_CACHE
+  if (( ${+__PROMPT_WORKING_DIR_BLOCK_CACHE["$PWD"]} == 1 )); then
+    # Full working dir block is in cache
+    echo "${__PROMPT_WORKING_DIR_BLOCK_CACHE["$PWD"]}"
+  else
+    # Just print current path
+    prompt_working_dir_part "$(shrink_path)"
+  fi
 }
 
 prompt_precmd() {
@@ -34,24 +41,31 @@ prompt_precmd() {
   async_init
   async_start_worker prompt -n
   async_register_callback prompt prompt_async_update_complete
-  async_job prompt prompt_async_update "$(pwd)" "$prompt_cmd_exec_time"
+  async_job prompt prompt_async_working_dir_block "$PWD"
 }
 add-zsh-hook precmd prompt_precmd
 
-# Render full prompt
-prompt_async_update() {
+# Render working dir block with git info in async worker
+prompt_async_working_dir_block() {
   cd "${1}" || return
-  typeset -g prompt_cmd_exec_time="$2"
-
-  echo "$(prompt_user_block)$(prompt_working_dir_block)"
-  echo -n "%_$(prompt_return_value_block)$(prompt_jobs_block)$(prompt_exec_time_block)$(prompt_docker_block)$(prompt_char) "
+  echo "$1"
+  prompt_working_dir_block
 }
 
 # Update prompt with async result
 prompt_async_update_complete() {
-  # shellcheck disable=SC2034
-  PROMPT="$3"
+  # Get output from worker
+  local output=("${(f)3}")
+  local working_dir="${output[1]}"
+  local working_dir_block="${output[2]}"
+
+  # Cache working dir block
+  typeset -gA __PROMPT_WORKING_DIR_BLOCK_CACHE
+  __PROMPT_WORKING_DIR_BLOCK_CACHE["$working_dir"]="$working_dir_block"
+
+  # Update prompt
+  # shellcheck disable=SC2016
+  PROMPT='$(prompt_user_block)$working_dir_block
+%_$(prompt_return_value_block)$(prompt_jobs_block)$(prompt_exec_time_block)$(prompt_docker_block)$(prompt_char) '
   zle && zle reset-prompt
 }
-
-# TODO: cache working dir block
